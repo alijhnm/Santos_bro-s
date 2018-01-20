c44793a2d1505e14ef7378bdbb36a4a6329882a4
alijhnm <ajahanmm@gmail.com>
2018-01-20 14:09:14 +0330

    added #1 Game,Edited Bazi adadi.py

--- pygame_test.py
+++ pygame_test.py
@@ -1,64 +1,43 @@
-import pygame,sys,random,time
+import pygame,sys,time
 import pygame.locals as GAME_GLOBALS
 import pygame.event as GAME_EVENTS
+import pygame.time as GAME_TIME
 pygame.init()
-window_width = 800
-window_height = 800
-surface = pygame.display.set_mode((window_width,window_height))
-pygame.display.set_caption('Pygame Mouse!')
-mouse_position = None
-mouse_pressed = False
-player_size = 40
-player_color = (255,255,255)
-player_x = window_height / 2
-player_y = window_height - player_size
-dragging = False
-gravity = 5
-def check_bounds():
-    global player_color,player_x,player_y,dragging
-    if mouse_pressed :
-        if player_x + player_size > mouse_position[0] > player_x:
-            if player_y + player_size > mouse_position[1] > player_y:
-                dragging = True
-                pygame.mouse.set_visible(0)
-    else :
-        player_color = (255,0,0)
-        pygame.mouse.set_visible(1)
-        dragging = False
-def check_gravity():
-    global gravity,player_y,player_size,window_height
-    if player_y < window_height - player_size and mouse_pressed == False:
-        player_y += gravity
-        gravity *= 1.1
-    else:
-        player_y = window_height - player_size
-        gravity = 5
-def draw_player():
-    global  player_color,player_x,player_y,dragging
-    if dragging:
-        player_color = (0,255,0)
-        player_x = mouse_position[0] - player_size / 2
-        player_y = mouse_position[1] - player_size / 2
-    pygame.draw.rect(surface,player_color,(player_x,player_y,player_size,player_size))
-def quitgame():
-    pygame.quit()
-    sys.exit()
-while True :
-    mouse_position = pygame.mouse.get_pos()
-    surface.fill((200,200,200))
-    if pygame.mouse.get_pressed()[0]:
-        mouse_pressed = True
-    else :
-        mouse_pressed = False
-    check_bounds()
-    check_gravity()
-    draw_player()
-    pygame.display.update()
-    time.sleep(0.02)
+class Board:
+
+    width = 800
+    height = 800
+    color = (0,0,0)
+    mid_color = (255,255,255)
+    mid_width = 40
+    mid_offset = 100
+    window = pygame.display.set_mode((width,height))
+
+    def show(self):
+        self.window.fill(self.color)
+        print('Done!')
+        pygame.draw.rect(self.window,self.mid_color,(self.mid_offset,self.height//2 - self.mid_width//2,self.width - 2 * self.mid_offset,self.mid_width))
+        pygame.display.update()
+        time.sleep(0.1)
+
+class Card():
+    
+def get_events():
     for event in GAME_EVENTS.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 quitgame()
-        if event.type == GAME_GLOBALS.QUIT:
-            quitgame()
-            
+
+def quitgame():
+    pygame.quit()
+    sys.exit()
+board = Board()
+
+# Main Loop
+
+while True:
+    get_events()
+    board.show()
+
+
+
